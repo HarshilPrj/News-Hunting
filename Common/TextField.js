a7febf2cf46d315b19c8caea1443293728410e8f
Jaydeep Solanki <53803660+jaydeep689@users.noreply.github.com>
2022-11-25 17:57:07 +0530

    commit

---
+++ Common/TextField.js
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./TextField.css";
+
+const TextField = ({ primary, backgroundColor, size, label, ...props }) => {
+  const mode = primary
+    ? "storybook-TextField--primary"
+    : "storybook-TextField--secondary";
+  return (
+    <input
+      type="text"
+      className={[
+        "storybook-TextField",
+        `storybook-TextField--${size}`,
+        mode,
+      ].join(" ")}
+      style={backgroundColor && { backgroundColor }}
+      {...props}
+    ></input>
+  );
+};
+
+export default TextField;
+
+TextField.propTypes = {
+  primary: PropTypes.bool,
+  backgroundColor: PropTypes.string,
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
+
+TextField.defaultProps = {
+  backgroundColor: null,
+  primary: false,
+  size: "medium",
+  onClick: undefined,
+};
